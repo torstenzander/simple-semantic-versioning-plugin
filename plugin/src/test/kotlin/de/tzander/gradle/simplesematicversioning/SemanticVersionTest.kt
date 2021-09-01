fd964892e622be1f6fbc19c9314c1b5ea1a866ff
Torsten Zander <info@tzander.de>
2021-09-01 18:16:06 +0200

    refactoring

--- plugin/src/test/kotlin/de/tzander/gradle/simplesematicversioning/SemanticVersionTest.kt
+++ plugin/src/test/kotlin/de/tzander/gradle/simplesematicversioning/SemanticVersionTest.kt
@@ -11,6 +11,7 @@ class SemanticVersionTest {
         val semanticVersion = SemanticVersion("12.2.1")
         assertEquals(12, semanticVersion.major)
         semanticVersion.increaseMajor()
+
         assertEquals("13.0.0", semanticVersion.newVersion())
     }
 
@@ -18,6 +19,7 @@ class SemanticVersionTest {
         val semanticVersion = SemanticVersion("12.2.1")
         assertEquals(2, semanticVersion.minor)
         semanticVersion.increaseMinor()
+
         assertEquals("12.3.0", semanticVersion.newVersion())
     }
 
@@ -25,35 +27,36 @@ class SemanticVersionTest {
         val semanticVersion = SemanticVersion("12.2.1")
         assertEquals(1, semanticVersion.patch)
         semanticVersion.increasePatch()
+
         assertEquals("12.2.2", semanticVersion.newVersion())
     }
 
     @Test fun `can handle snapshot patch version`() {
         val semanticVersion = SemanticVersion("12.2.1-SNAPSHOT")
-        assertEquals(1, semanticVersion.patch)
         semanticVersion.increasePatch()
+
         assertEquals("12.2.2-SNAPSHOT", semanticVersion.newVersion())
     }
 
     @Test fun `can handle snapshot minor version`() {
         val semanticVersion = SemanticVersion("12.2.1-SNAPSHOT")
-        assertEquals(1, semanticVersion.patch)
         semanticVersion.increaseMinor()
+
         assertEquals("12.3.0-SNAPSHOT", semanticVersion.newVersion())
     }
 
     @Test fun `can handle release with dot`() {
         val semanticVersion = SemanticVersion("12.2.1.RELEASE")
-        assertEquals(1, semanticVersion.patch)
         semanticVersion.increaseMinor()
+
         assertEquals("12.3.0.RELEASE", semanticVersion.newVersion())
     }
 
     @Test fun `can not handle release with hashtag`() {
         assertFailsWith<IllegalArgumentException> {
             val semanticVersion = SemanticVersion("12.2.1#RELEASE")
-            assertEquals(1, semanticVersion.patch)
             semanticVersion.increaseMinor()
+
             assertNotEquals("12.3.0#RELEASE", semanticVersion.newVersion())
         }
     }
